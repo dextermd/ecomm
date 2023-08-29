3d7e219627292e009435f5bf8bd09e184828cb2e
dextermd <dextermdpay@gmail.com>
2023-08-29 18:06:24 +0300

    Profile adding

---
+++ resources/views/admin/layouts/sidebar.blade.php
@@ -0,0 +1,22 @@
+<div class="main-sidebar sidebar-style-2">
+    <aside id="sidebar-wrapper">
+        <div class="sidebar-brand">
+            <a href="#">DEXTERMD</a>
+        </div>
+        <div class="sidebar-brand sidebar-brand-sm">
+            <a href="#">DMD</a>
+        </div>
+        <ul class="sidebar-menu">
+            <li class="menu-header"></li>
+
+            <li><a class="nav-link" href="{{route('admin.dashboard')}}"><i class="far fa-square"></i> <span>Dashboard</span></a></li>
+        </ul>
+
+        <div class="mt-4 mb-4 p-3 hide-sidebar-mini">
+            <a href="https://getstisla.com/docs" class="btn btn-primary btn-lg btn-block btn-icon-split">
+                DEXTERMD
+            </a>
+        </div>
+    </aside>
+</div>
+
--- routes/admin.php
+++ routes/admin.php
@@ -1,8 +1,12 @@
 <?php
 
 use App\Http\Controllers\Backend\AdminController;
+use App\Http\Controllers\Backend\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 
 /* Admin Routes */
 Route::get('dashboard', [AdminController::class, 'dashboard'])->name('dashboard');
+
+/* Profile Routes */
+Route::get('profile', [ProfileController::class, 'index'])->name('profile');
