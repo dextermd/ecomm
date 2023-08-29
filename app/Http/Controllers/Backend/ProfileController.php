b0e12f81432a7c2d547fbe9ce0b5f6941396e706
dextermd <dextermdpay@gmail.com>
2023-08-29 23:36:56 +0300

    front template

--- app/Http/Controllers/Backend/ProfileController.php
+++ app/Http/Controllers/Backend/ProfileController.php
@@ -4,11 +4,65 @@ namespace App\Http\Controllers\Backend;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use File;
 
 class ProfileController extends Controller
 {
     public function index()
     {
         return view('admin.profile.index');
+    }
+
+    public function updateProfile(Request $request)
+    {
+        $request->validate([
+            'name' => ['required', 'max:100'],
+            'email' => ['required', 'email', 'unique:users,email,'.Auth::user()->id],
+            'image' => ['image', 'max:2048']
+        ]);
+
+        $user = Auth::user();
+
+        if ($request->hasFile('image')){
+            if (File::exists(public_path($user->image))){
+                File::delete(public_path($user->image));
+            }
+            $image = $request->image;
+            $imageName = rand().'_userId-'.$user->id.'_'.$image->getClientOriginalName();
+            $image->move(public_path('uploads/users'), $imageName);
+
+            $path = "/uploads/users/".$imageName;
+
+            $user->image = $path;
+        }
+
+        $user->name = $request->name;
+        $user->email = $request->email;
+        $user->save();
+
+        toastr()->success('Profile Updated Successfully!');
+        return redirect()->back();
+    }
+
+    /* Update Password */
+    public function updatePassword(Request $request)
+    {
+        $request->validate([
+            'current_password' => ['required', 'current_password'],
+            'password' => ['required', 'confirmed', 'min:8'],
+        ]);
+
+        if ($request->user()->update([
+            'password' => bcrypt($request->password)
+        ])) {
+            auth()->logout();
+            toastr()->success('Profile Password Updated Successfully!');
+            return redirect(route('admin.login'));
+        } else {
+            // Обновление пароля не удалось
+            toastr()->error('Не удалось обновить пароль');
+            return redirect()->back()->with('error', 'Не удалось обновить пароль');
+        }
     }
 }
--- resources/views/admin/profile/index.blade.php
+++ resources/views/admin/profile/index.blade.php
@@ -10,25 +10,34 @@
             </div>
         </div>
         <div class="section-body">
+
             <div class="row mt-sm-4">
+
                 <div class="col-12 col-md-12 col-lg-7">
                     <div class="card">
-                        <form method="post" class="needs-validation" novalidate="">
+                        <form method="POST" class="needs-validation" novalidate="" action="{{route('admin.profile.update')}}" enctype="multipart/form-data">
+                            @csrf
                             <div class="card-header">
                                 <h4>Update Profile</h4>
                             </div>
                             <div class="card-body">
                                 <div class="row">
+                                    <div class="form-group col-12">
+                                        <div class="mb-3">
+                                            <img width="100px" src="{{asset(Auth::user()->image)}}" alt="">
+                                        </div>
+                                        <label>Image</label>
+                                        <input type="file" name="image" class="form-control" value="">
+                                    </div>
+
                                     <div class="form-group col-md-6 col-12">
                                         <label>Name</label>
-                                        <input type="text" class="form-control" value="" required="">
+                                        <input type="text" name="name" class="form-control" value="{{Auth::user()->name}}">
                                     </div>
+
                                     <div class="form-group col-md-6 col-12">
-                                        <label>Last Name</label>
-                                        <input type="text" class="form-control" value="Maman" required="">
-                                        <div class="invalid-feedback">
-                                            Please fill in the last name
-                                        </div>
+                                        <label>Email</label>
+                                        <input type="text" name="email" class="form-control" value="{{Auth::user()->email}}">
                                     </div>
                                 </div>
 
@@ -39,6 +48,42 @@
                         </form>
                     </div>
                 </div>
+
+
+                <div class="col-12 col-md-12 col-lg-7">
+                    <div class="card">
+                        <form method="POST" class="needs-validation" novalidate="" action="{{route('admin.password.update')}}" enctype="multipart/form-data">
+                            @csrf
+                            <div class="card-header">
+                                <h4>Update Password</h4>
+                            </div>
+                            <div class="card-body">
+                                <div class="row">
+
+                                    <div class="form-group col-12">
+                                        <label>Current Password</label>
+                                        <input type="password" name="current_password" class="form-control">
+                                    </div>
+
+                                    <div class="form-group col-12">
+                                        <label>New Password</label>
+                                        <input type="password" name="password" class="form-control">
+                                    </div>
+
+                                    <div class="form-group col-12">
+                                        <label>Confirm New Password</label>
+                                        <input type="password" name="password_confirmation" class="form-control">
+                                    </div>
+                                </div>
+
+                            </div>
+                            <div class="card-footer text-right">
+                                <button class="btn btn-primary">Save Changes</button>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+
             </div>
         </div>
     </section>
